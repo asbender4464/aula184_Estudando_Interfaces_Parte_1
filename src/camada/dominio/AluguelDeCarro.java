7859c81d890c3fb048fe85446beb6ac253f26238
Andre Silva Bender <asbender4464@gmail.com>
2019-11-02 12:02:53 -0300

    Versão final, comentada.

--- src/camada/dominio/AluguelDeCarro.java
+++ src/camada/dominio/AluguelDeCarro.java
@@ -5,7 +5,56 @@ import java.util.Date;
 public class AluguelDeCarro {
 
 	//Atributos
-	Date inicio;
-	Date fim;
+	private Date inicio;
+	private Date fim;
 	
+	//Estabelecendo as associações
+	private Veiculo veiculo;
+	private NotaFiscal notaFiscal;
+	
+	//Construtor padrão.
+	public AluguelDeCarro() {
+	}
+
+	//Construtor com argumentos.
+	//Neste construtor 'não há' o argumento 'notaFiscal' pois, de acordo com o diagrama UML,
+	//a NF poderá ou não ter sido gerada.
+	public AluguelDeCarro(Date inicio, Date fim, Veiculo veiculo) {
+		this.inicio = inicio;
+		this.fim = fim;
+		this.veiculo = veiculo;
+	}
+
+	//GETs e SETs
+	public Date getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Date inicio) {
+		this.inicio = inicio;
+	}
+
+	public Date getFim() {
+		return fim;
+	}
+
+	public void setFim(Date fim) {
+		this.fim = fim;
+	}
+
+	public Veiculo getVeiculo() {
+		return veiculo;
+	}
+
+	public void setVeiculo(Veiculo veiculo) {
+		this.veiculo = veiculo;
+	}
+
+	public NotaFiscal getNotaFiscal() {
+		return notaFiscal;
+	}
+
+	public void setNotaFiscal(NotaFiscal notaFiscal) {
+		this.notaFiscal = notaFiscal;
+	}
 }
--- src/camada/dominio/Veiculo.java
+++ src/camada/dominio/Veiculo.java
@@ -3,7 +3,23 @@ package camada.dominio;
 public class Veiculo {
 
 	//Atributos
-	String modelo;
+	private String modelo;
 	
-	
+	//Construtor padrão
+	public Veiculo() {
+	}
+
+	//Construtor com argumentos
+	public Veiculo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	//GET e SET
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
 }
